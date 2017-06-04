ab09f2e65f759f54a403462c0f3c361171502c2a
Aaron DC <aaronbdc@mail.com>
2017-06-04 08:38:34 -0400

    Initial commit.

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+//class main extends Component {
+//
+//}
+//export default main
+module.exports = React.createClass({
+  render(){
+    return(
+      <View>
+        <Text>
+          To-Do Master
+        </Text>
+      </View>
+    )
+
+  }
+})
